8948434472f915984874fbb92c0759a16f0a45d2
InkonIo <loke90@bk.ru>
2025-03-30 19:50:38 +0300

    lasto4

--- src/main/java/guis/PersonalAccount.java
+++ src/main/java/guis/PersonalAccount.java
@@ -10,7 +10,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
